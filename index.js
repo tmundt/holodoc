7469722dbb18ff54d08db863a8a3967c8b98f696
muth <muth@mb-muth.local>
2016-09-06 18:14:13 +0200

    changed config file, Bot is responding

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ var DocBot = require('./lib/DocBot');
  *  BOT_DB_PATH: the path of the SQLite database used by the bot
  *  BOT_NAME: the username you want to give to the bot within your organisation.
  */
-var token = process.env.BOT_API_KEY || require('./token');
+var token = process.env.BOT_API_KEY || require('./config').token.docBot;
 //var dbPath = process.env.BOT_DB_PATH ||'some/path';
-var name = process.env.BOT_NAME||'holodoc';
+var name = process.env.BOT_NAME||require('./config').name.docBot||'holodoc';
 
 console.log('index.js: name: ' + name);
 
